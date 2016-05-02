8d63181564e391d05282c074eab0318d47ea9b73
david63 <david.wood@pear-tree.org.uk>
2016-05-02 21:20:35 +0100

    Re commit

---
+++ migrations/version_1_0_1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package Joined Date Format Extension
+* @copyright (c) 2015 david63
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace david63\joineddate\migrations;
+
+class version_1_0_1 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\david63\joineddate\migrations\version_1_0_0');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.remove', array('version_joineddate')),
+		);
+	}
+}
